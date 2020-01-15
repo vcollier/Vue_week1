040305ef59cf92dbf2c302d7080d2919611dc8ee
Collier <v_collier@ats-hr420-dm23.fc.ca>
2020-01-15 09:43:49 -0500

    adding navbar

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// todo => use a key to track the current video, or just pass the video in as a ref to the function and grab its source
+
+var vm = new Vue({
+  el: "#app",
+
+  data: {
+
+    // mock up the user - this well eventually come from the database UMS (user management system)
+    user: {
+      isAdmin: false,
+      avatar: null,
+      isLoggedIn: true
+    },
+
+
+    // this data would also come from the database, but we'll just mock it up for now
+    videodata: [
+      { name: "Star Wars The Force Awakens", thumb: "forceawakens.jpg", vidsource: "forceawakens.mp4", description: "yet another star wars movie" },
+      { name: "Stranger Things", thumb: "strangerthings.jpg", vidsource: "strangerthings.mp4", description: "don't get lost in the upside down" },
+      { name: "Marvel's The Avengers", thumb: "avengers.jpg", vidsource: "avengers.mp4", description: "will they make black widow action figures this time?" }
+    ],
+
+    showDetails: false
+  },
+
+  methods: {
+    setUserPrefs() {
+      // this is the preferences control, hit the api when ready (or use a component)
+      console.log('set user prefs here');
+    },
+
+    userLogin() {
+      // call the login route, or load the login component
+      console.log('do login / logout on click');
+
+      // this is a ternary statement -> shorthand for if / else
+      // the expression evaute to true or false - if it's true, set the value equal to
+      // the left of the colon. if it's false, set the value equal to the right.
+      this.user.isLoggedIn = (this.user.isLoggedIn) ? false : true;
+    }
+
+  }
+});
